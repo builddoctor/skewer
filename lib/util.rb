68d3e0b95ef657b17fc99977a4edf4768ff43ef5
Kushal Pisavadia <kushi.p@gmail.com>
2012-03-07 12:59:56 +0000

    fix up spacing

--- lib/util.rb
+++ lib/util.rb
@@ -7,14 +7,10 @@ module Skewer
     def get_location(server = nil)
       # Negative case.
       return nil if server.nil?
-
       return nil if not server.respond_to?(:dns_name) and not server.respond_to?(:public_ip_address)
 
       # Positive case.
-
-
       return server.dns_name if server.respond_to? :dns_name
-
       return server.public_ip_address if server.respond_to? :public_ip_address
     end
   end
