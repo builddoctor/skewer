91bc0571e94491b268c535939a1fe12f143de156
Kushal Pisavadia <kushi.p@gmail.com>
2012-03-13 10:56:05 +0000

    Add tests to exercise the module-based logger
    These tests just make sure that the methods exposed at the module-level
    for logging are exercised, as they are important the execution of
        Skewer.

---
+++ spec/logger_spec.rb
@@ -0,0 +1,19 @@
+require 'skewer'
+
+describe Skewer do
+  it "should have a logger object available on the module" do
+    Skewer.logger.nil?.should == false
+    Skewer.logger.class.should == Logger
+  end
+
+  it "should have a way to inject a new logger" do
+    # Check that a Logger already exists.
+    Skewer.logger.nil?.should == false
+    Skewer.logger.class.should == Logger
+
+    # Override the logger.
+    Skewer.logger = String.new
+    Skewer.logger.nil?.should == false
+    Skewer.logger.class.should == String
+  end
+end
